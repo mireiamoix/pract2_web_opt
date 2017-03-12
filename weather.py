86447afd1440a83a026d8195ba23a3824428ec65
mireiamoix <mmoix.lestonnac@gmail.com>
2017-03-12 12:50:40 +0100

    prediction + consells DONE

--- weather.py
+++ weather.py
@@ -33,16 +33,29 @@ class WeatherHourly(object):
         forecast = soup.find_all("forecast")
 
         max_hores = proximes_hores
-        j = 0
-        for i in forecast:
+        j = 0  # variable per controlar les hores que es volen
+
+        for i in forecast:  # tantes voltes com max_hores passades
             if j >= int(max_hores):
                 break
 
             hora = "A les " + i.find("hour").text
             temperatura = " la temperatura sera " + i.find("temp").find("metric").text
-            sensacio_temp = ". Pero amb una sensacio termica de " + (i.find("feelslike").find("metric").text)
-            cel = " i amb un cel " + i.find("wx").text
-            print hora + temperatura + sensacio_temp + cel
+            sensacio_temp = (i.find("feelslike").find("metric").text)
+            sensacio_temp_frase = ". Pero amb una sensacio termica de " + sensacio_temp
+            cel = i.find("wx").text
+            cel_frase = " i amb un cel " + cel
+            print hora + temperatura + sensacio_temp_frase + cel_frase
+
+            if (cel == "Mostly Clear" or cel == "Clear" or cel == 'Sunny' or cel == "Mostly Sunny"):
+                if (int(sensacio_temp) > 22):
+                    print "  CONSELL: si portes jaqueta et sobrara.\n"
+                print "  CONSELL: pots portar jaqueta per si refresca.\n"
+
+            if (cel == "Partly Cloudy" or cel == "Cloudy" or cel == "Rainy" or cel == "Mostly Rainy"):
+                if int(sensacio_temp) > 25:
+                    print "  CONSELL: Porta paraigües que ploura, pero jaqueta no cal.\n"
+                print "  CONSELL: Porta paraigües i jaqueta per si refresca.\n"
 
             j = j+1
 
@@ -55,6 +68,6 @@ if __name__ == "__main__":
         except IndexError:
             print "API key must be in CLI option"
 
-    print "Es tindra una prediccio de les proximes " + proximes_hores + " hores."
+    print "Es tindra una prediccio de les proximes " + proximes_hores + " hores.\n"
     wc = WeatherHourly(api_key)
     wc.hourly("Lleida")
